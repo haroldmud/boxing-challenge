b108ce71b32d0802998f20c36a97fdf3c11a0f35
haroldmud <haroldmudosa@gmail.com>
2023-01-30 17:01:23 +0200

    Feat: Finalize the project

---
+++ components/box.jsx
@@ -0,0 +1,11 @@
+// import { useState } from "react"
+// import data from "./data";
+
+// export default function Box(props){
+//   
+//   console.log(data[0].bg)
+
+//   return(
+//     <div onClick={props.boo}  className={`h-32 w-32 border-2 ${props.bool ? "bg-fuchsia-500" : "" }`}>{props.text}</div>
+//   )
+// }
---
+++ components/data.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    text:"",
+    bg: true,
+  },
+  {
+    text:"",
+    bg: false,
+  },
+  {
+    text:"",
+    bg: true,
+  },
+  {
+    text:"",
+    bg: true,
+  },
+  {
+    text:"",
+    bg: false,
+  },
+  {
+    text:"",
+    bg: false,
+  }]
+  
