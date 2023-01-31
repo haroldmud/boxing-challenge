e727788d81ee23d9b24b9dbd40d045ea9fef6c96
haroldmud <haroldmudosa@gmail.com>
2023-01-31 10:51:59 +0200

    Fix: Utilize the derivated state

--- components/data.js
+++ components/data.js
@@ -1,25 +1,31 @@
 export default [
   {
+    id:1,
     text:"",
     bg: true,
   },
   {
+    id:2,
     text:"",
     bg: false,
   },
   {
+    id:3,
     text:"",
     bg: true,
   },
   {
+    id:4,
     text:"",
     bg: true,
   },
   {
+    id:5,
     text:"",
     bg: false,
   },
   {
+    id:6,
     text:"",
     bg: false,
   }]
--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,18 @@ import data from "../components/data"
 
 export default function Home(){
   const [clicked, setClicked] = useState(data);
-  const [bool, setBool] = useState(true);
+  const [on, setOn] = useState(true);
+
   return(      
     <section className="flex justify-center mt-12">
         <div className="grid grid-cols-3 gap-4">
           {
-            clicked.map((w,i)=><div key={i} onClick={()=>{
-              w.bg = !w.bg
-              setBool(current=>!current);
-            }} className={`h-32 w-32 border ${w.bg === true ? "bg-fuchsia-500" :"bg-fuchsia-100" }`}>{w.text}</div>)
+            clicked.map((w,i)=><div key={i} onClick={
+              ()=>{
+                w.bg = !w.bg;
+                setOn(current => !current)
+              }
+            } className={`h-32 w-32 rounded-xl border-2 border-black ${w.bg === true ? "bg-fuchsia-500" :"bg-fuchsia-100" }`}>{w.text}</div>)
           }
         </div>
       </section>
